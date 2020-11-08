1aab34f7a35b0c6021b7082bde3c6e72d6bc65e2
timurunaspekov <timur.unaspekov@nu.edu.kz>
2020-11-08 19:28:40 +0600

    New assets added

---
+++ Unity Project/Assets/Scripts For Game/MoneyBag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class MoneyBag : MonoBehaviour
+{
+    public Text text;
+
+    void SetMaxMoney(int maxMoney)
+    {
+        text.text = "0/" + maxMoney;
+    }
+
+    void SetMoney(int CurrentMoney, int MaxMoney)
+    {
+        text.text = CurrentMoney + "/" + MaxMoney;
+    }
+}
--- Unity Project/Assets/Scripts For Game/PlayerAttributes.cs
+++ Unity Project/Assets/Scripts For Game/PlayerAttributes.cs
@@ -6,14 +6,18 @@ public class PlayerAttributes : MonoBehaviour
 {
     [SerializeField] private int MaxHealth = 100;
     [SerializeField] public int CurrentHealth;
+    [SerializeField] public int MaxMoney = 20;
+    [SerializeField] public int CurrentMoney = 0;
 
     public HealthBarScript healthBar;
+    public MoneyBag moneyBag;
 
     // Start is called before the first frame update
     void Start()
     {
         CurrentHealth = MaxHealth;
         healthBar.SetMaxHealth(MaxHealth);
+        moneyBag.SetMaxMoney(MaxMoney);
     }
 
     // Update is called once per frame
@@ -22,7 +26,15 @@ public class PlayerAttributes : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(5);
+            GetCoin(1);
         }
+
+    }
+
+    void GetCoin(int value)
+    {
+        CurrentMoney += value;
+        moneyBag.SetMoney(CurrentMoney);
     }
 
     void TakeDamage(int damage)
