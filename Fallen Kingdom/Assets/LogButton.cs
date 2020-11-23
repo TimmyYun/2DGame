4e47a9897bf0d17df715547d157715fd265e24bd
timurunaspekov <timur.unaspekov@nu.edu.kz>
2020-11-23 09:40:42 +0600

    Update 2.1

---
+++ Fallen Kingdom/Assets/LogButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LogButton : MonoBehaviour
+{
+    public int currentLog = 0;
+    public int maxLog = 10;
+    public TMP_Text text;
+    public void ChangeLogCount(int value)
+    {
+        currentLog += value;
+        text.
+    }
+}
